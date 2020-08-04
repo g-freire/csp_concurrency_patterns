fbe9f9aeee0e7d8c1806b1340e44f6a8d441a2f4
g-freire <gustavomfreire@gmail.com>
2020-08-04 05:16:10 -0300

    fix goroutines func

--- goroutines.go
+++ goroutines.go
@@ -9,10 +9,9 @@ import (
 
 /*
 GOROUTINES SYNCRONIZATION
-
 When we use goroutines the main function has to wait for the scheduled goroutines.
 Below, we demonstrate that the main func finishes execution before executing the
-scheduled goroutine
+scheduled goroutine and then an example using a sleep approach, giving some deterministic time for the scheduled execution
  */
 
 func NotSyncExample(){
@@ -21,22 +20,18 @@ func NotSyncExample(){
 	}("Calling the lambda without sync \n")
 }
 
-// Next, example using a sleep approach, giving some deterministic time for the scheduled execution
-
 func SleepExample(){
 	go func(msg string){
 		println(msg)
-	}("Calling the lambda with arguments \n")
+	}("Calling the lambda with some delay \n")
 	time.Sleep(time.Second)
 }
-
 
 /*
 Next we use waitgroups primitives, since it is more cost efficient than sleep (waits only the necessary time),
 within a anonymous function to syncronize the main goroutine with the scheduled ones.
 Is interesting to note that concurrent application does not guarantee the order of execution,
 the OS manages the threads priorities.
-
  */
 
 func WaitGroupExample() {
@@ -46,14 +41,15 @@ func WaitGroupExample() {
 
 	for i :=0; i < goRoutines; i++{
 		go func(goRoutineID int){
-			fmt.Printf("ID:%d: Hello goroutines!\n", goRoutineID)
+			fmt.Printf("ID:%d: Hello waited goroutine ID: \n", goRoutineID)
 			wait.Done() //subtract one wait entity - same as -1
 		}(i)
 	}
 	wait.Wait() // this is probably executed before the goroutines
 }
 
-func main(){
-	//AnonymousExample()
-	//WaitGroupExample()
-}
+// func main(){
+	// NotSyncExample()
+	// AnonymousExample()
+	// WaitGroupExample()
+// }
