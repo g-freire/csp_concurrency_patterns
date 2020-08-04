21919cde531823b6478b0ff1c41e2249a7a3bdde
g-freire <gustavomfreire@gmail.com>
2020-08-04 05:10:24 -0300

    add readme and sync examples

---
+++ README.md
@@ -0,0 +1,8 @@
+There are many concurrency models in CS, the most fasmous being the ACTOR model present in languages such as Erlang and Scala. Go, on the other side, uses CSP - Communication Sequential Processes, which has a different approach to concurrency.
+
+In Actor model, the communication needs to be stablish between the entites beforehand, during creation (eg. the process IDs) and then the non-blocking data transmition (through message queues) can happens. 
+CSP on the other side introduces a way to comunicate anonyumously, using channels ID's indepentends. So, this abstraction does not tell how many processes are on each side of the channel.
+
+Concurrency enables parallelism by designin a correct structure of concurrency work.
+The key thing is that with a concurrent design, we don`t have to worry about parallelism, we can think about it as an extra feature if our concurrent design is correct.
+An analogy would be, that our nervous systems are channels, and we dont care about how many legs and arms we could control
---
+++ goroutines.go
@@ -0,0 +1,59 @@
+//package go_csp
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+/*
+GOROUTINES SYNCRONIZATION
+
+When we use goroutines the main function has to wait for the scheduled goroutines.
+Below, we demonstrate that the main func finishes execution before executing the
+scheduled goroutine
+ */
+
+func NotSyncExample(){
+	go func(msg string){
+		println(msg)
+	}("Calling the lambda without sync \n")
+}
+
+// Next, example using a sleep approach, giving some deterministic time for the scheduled execution
+
+func SleepExample(){
+	go func(msg string){
+		println(msg)
+	}("Calling the lambda with arguments \n")
+	time.Sleep(time.Second)
+}
+
+
+/*
+Next we use waitgroups primitives, since it is more cost efficient than sleep (waits only the necessary time),
+within a anonymous function to syncronize the main goroutine with the scheduled ones.
+Is interesting to note that concurrent application does not guarantee the order of execution,
+the OS manages the threads priorities.
+
+ */
+
+func WaitGroupExample() {
+	var wait sync.WaitGroup
+	goRoutines := 5
+	wait.Add(goRoutines) //add one wait entity - same as +1
+
+	for i :=0; i < goRoutines; i++{
+		go func(goRoutineID int){
+			fmt.Printf("ID:%d: Hello goroutines!\n", goRoutineID)
+			wait.Done() //subtract one wait entity - same as -1
+		}(i)
+	}
+	wait.Wait() // this is probably executed before the goroutines
+}
+
+func main(){
+	//AnonymousExample()
+	//WaitGroupExample()
+}
